6aa7155d340c9c4d5fecc121cfbfc24cb627201a
Idrees-02 <      GitHub>
2026-07-04 11:55:40 +0300

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import { useState, useMemo } from 'react';
 import { getTriagedRequests } from './data';
 import { RequestCard } from './components/RequestCard';
 import './App.css';
